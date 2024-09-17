3dd9e26ac397942cad860a9f4afc9f849af4163e
Myles Lewis <myles.lewis@qmul.ac.uk>
2024-09-17 06:36:18 +0100

    Add documentation

--- R/cat_parallel.R
+++ R/cat_parallel.R
@@ -1,9 +1,23 @@
 # prints using shell echo from inside mclapply when run in Rstudio
+
+#' Versions of `cat` and `message` for parallel processing
+#' 
+#' Prints messages to the console using `echo` during to enable messages to be
+#' printed during parallel processing. Text is only printed if the Rstudio
+#' environment is detected.
+#' 
+#' @param ... zero or more objects which can be coerced to character and which
+#'   are pasted together.
+#' @return Prints a message to the console. `cat_parallel` uses no line feed,
+#'   while `message_parallel` always adds a newline.
+#' @export
 cat_parallel <- function(...) {
   if (!checkenv()) return()
   system(sprintf('echo "%s', paste0(..., '\\c"', collapse = "")))
 }
 
+#' @rdname cat_parallel
+#' @export
 message_parallel <- function(...) {
   if (!checkenv()) return()
   system(sprintf('echo "%s"', paste0(..., collapse = "")))
@@ -19,11 +33,11 @@ over_parallel <- function(...) {
 #' 
 #' `mcstop` is a multicore version of `stop` which prints to the console using
 #' `echo` during parallel commands such as [mclapply()], to allow error messages
-#' to be seen more clearly.
+#' to be more visible.
 #' 
 #' @param ... Objects coerced to character and pasted together and printed to
 #'   the console using `echo`.
-#' @return Produces an error message.
+#' @return Prints an error message.
 #' @export
 mcstop <- function(...) {
   system(sprintf('echo "%s"', paste0('\\nError: ', ..., collapse = "")))
